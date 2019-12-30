941fb52507e1f896813d62d91e16eaf91ae3924b
Cristian Aldea <aldea.cristian@outlook.com>
2019-12-30 14:20:00 -0500

    Add java args to bash minecraft server script

--- minecraft/run_server.sh
+++ minecraft/run_server.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+
 java -Xms6G -Xmx6G -jar forge.jar nogui
+#java -XX:+UseG1GC -Xms6G -Xmx6G -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M forge.jar nogui
