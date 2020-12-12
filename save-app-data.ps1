bedcfea32dc7a99a64f7f4eaca444678b09ac7ab
Cristian Aldea <aldea.cristian@outlook.com>
2020-12-12 13:09:46 -0500

    Update save-app-data.ps1

--- save-app-data.ps1
+++ save-app-data.ps1
@@ -4,7 +4,8 @@ $pathsToCopy = "\AppData\LocalLow\Team Cherry",
 "\AppData\Roaming\DarkSoulsIII", 
 "\AppData\Roaming\Factorio",
 "\AppData\Roaming\dungeon",
-"\AppData\Local\HyperLightDrifter"
+"\AppData\Local\HyperLightDrifter",
+"\AppData\Local\Downwell_v1_0_5"
 
 foreach ($path in $pathsToCopy) {
     if(Test-Path "$appdataFolder$path") {
