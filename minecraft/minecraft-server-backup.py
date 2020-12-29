f270d66c963a2f80d9b96e92c685af3fea937511
Cristian Aldea <aldea.cristian@outlook.com>
2020-12-28 20:12:36 -0500

    Allow backing up worlds of paper servers

--- minecraft/minecraft-server-backup.py
+++ minecraft/minecraft-server-backup.py
@@ -3,27 +3,49 @@ import os
 import datetime
 import shutil
 
+
+def backupFolder(source, target):
+    if os.path.exists(source):
+        print("Saving backup of \"{}\" to \"{}\"".format(source, target))
+        shutil.copytree(source, target)
+    else:
+        print("Could not find \"{}\"".format(source))
+
+
 # Command-line arguments
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--source', required=True,
                     help="The location of the minecraft world folder to backup")
 parser.add_argument('--target', required=True,
                     help="The location where to save backups of the given world folder")
-parser.add_argument('--num_backups', required=False, default=8,
+parser.add_argument('--num_backups', required=False, default=10,
                     help="The number of backups to keep")
+parser.add_argument('--paper', '-p', required=False, default=False, action='store_true',
+                    help="If the current backup is being done for a paper server, which stores the world save in a slightly different format")
 args = parser.parse_args()
 
+print("\nArguments: {}".format(args))
+
 if not os.path.exists(args.target):
-    print("Creating diretory {}".format(args.target))
+    print("\nCreating directory {}".format(args.target))
     os.makedirs(args.target)
 
 # Creating path name for new backup
 timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S:%f")
 basename = os.path.basename(args.source)
-backup_path = "{}/{}_{}".format(args.target, basename, timestamp)
+backup_path = "{}/{}/{}".format(args.target, timestamp, basename)
 
-print("\nSaving backup of \"{}\" to \"{}\"".format(args.source, backup_path))
-shutil.copytree(args.source, backup_path)
+print()
+backupFolder(args.source, backup_path)
+
+if args.paper:
+    nether_suffix = '_nether'
+    end_suffix = '_the_end'
+    print('Paper flag set. Attempting to save nether and end folders.')
+
+    backupFolder(args.source + nether_suffix, backup_path + nether_suffix)
+    backupFolder(args.source + end_suffix, backup_path + end_suffix)
+
 
 # Listing currently saved backups
 backups = os.listdir(args.target)
