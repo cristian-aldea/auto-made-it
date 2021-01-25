4cbbc5b60e3aeb02b0857888c9f3bff08e2229c9
Cristian Aldea <aldea.cristian@outlook.com>
2021-01-25 17:00:24 -0500

    Add discord fix script

---
+++ discord-fix.sh
@@ -0,0 +1,9 @@
+vlc \
+    --no-video-deco \
+    --no-embedded-video \
+    --screen-fps=30 \
+    --screen-top=0 \
+    --screen-left=0 \
+    --screen-width=2560 \
+    --screen-height=1440 \
+    screen://
